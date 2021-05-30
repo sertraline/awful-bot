060e4577e8c34ec2751b8a296977af326447207d
sertraline <36751007+sertraline@users.noreply.github.com>
2021-05-30 13:24:39 +0300

    Add youtube search module

---
+++ core/modules/youtube_search.py
@@ -0,0 +1,77 @@
+import aiohttp
+import json
+import random
+import traceback
+import re
+
+
+class Executor:
+    command = 'youtube'
+    use_call_name = False
+
+    __url = 'https://www.youtube.com/results'
+
+    def __init__(self, config, debugger):
+        self.config = config
+        self.debug = debugger
+
+    def help(self):
+        return "Youtube search: \n %s <song name>" % self.command
+
+    async def search(self, name):
+
+        params = {
+            'search_query': name
+        }
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
+        }
+        cookies = {
+            'CONSENT': 'YES+cb.20210328-17-p0.en-GB+FX+{}'.format(random.randint(100, 999))
+        }
+
+        async with aiohttp.ClientSession(headers=headers, cookies=cookies) as session:
+            async with session.get(self.__url, params=params) as r:
+                if r.status != 200:
+                    self.debug('Youtube search: %d' % r.status)
+                    return
+
+                resp = await r.text()
+                data = re.findall(r'var ytInitialData = ({.*?});', resp)
+                if not data:
+                    self.debug('Youtube search: no data')
+                    return
+
+                js = json.loads(data[0])
+                renderer = js['contents']['twoColumnSearchResultsRenderer']['primaryContents']['sectionListRenderer']
+                contents = renderer['contents'][0]['itemSectionRenderer']['contents']
+
+                results = ''
+                for content in contents[:6]:
+                    if 'videoRenderer' not in content:
+                        continue
+                    renderer = content['videoRenderer']
+                    video_id = renderer['videoId']
+                    title = renderer['title']['runs'][0]['text']
+                    length = renderer['lengthText']['simpleText']
+                    result = '%s [%s]\n%s\n' % (title, length, 'https://youtube.com/watch?v=' + video_id)
+                    results += result
+
+                return results
+
+    async def call_executor(self, event):
+        message = event.raw_text.replace('%syoutube' % self.config.S, '').strip()
+        if not message:
+            return
+
+        result = None
+        try:
+            result = await self.search(message)
+        except:
+            self.debug(traceback.format_exc())
+            pass
+
+        if result:
+            await event.reply(result)
+        else:
+            await event.reply("No results for this query.")
